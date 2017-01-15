3d44c00248e266a9ce356d30019b7162c620ea36
scott.underwood87 <scott.underwood87@gmail.com>
2017-01-15 11:57:17 -0500

    end stage one of flask-socketio integration

--- manage.py
+++ manage.py
@@ -4,10 +4,14 @@ from app import create_app, db
 from app.models import User, Role
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
+#socket test (1 line)
+from flask_socketio import SocketIO, emit
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
+#socket test (1 line)
+socketio = SocketIO(app)
 
 
 def make_shell_context():
@@ -23,6 +27,16 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+    
+#next two flask socket tests
+@socketio.on('connect', namespace='/')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect', namespace='/')
+def test_disconnect():
+    print('Client disconnected')
 
 if __name__ == '__main__':
-    manager.run()
+    #socketio changed from: manager.run()
+    socketio.run(app, host='0.0.0.0')
